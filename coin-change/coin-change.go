2c181392855182d507a8c4d14e996199fefd8718
Mouhamet Ndiaye <ahmetndiaye404@gmail.com>
2022-12-08 19:04:04 +0000

    coin change part-3

--- coin-change/coin-change.go
+++ coin-change/coin-change.go
@@ -21,31 +21,31 @@ func main() {
 	// part 1: normal coin change
 	{
 		/*
-			Problem:
-				Coin change
+			 Problem:
+				 Coin change
 
-				Given an unlimited supply of coins of given denominations(j), find the total number of ways(n) to make a change of size n.
+				 Given an unlimited supply of coins of given denominations(j), find the total number of ways(n) to make a change of size n.
 
-				denominations: 1,3,5,10
-				1. Objective function
-					f(i) return the different number of ways to make change of size n.
+				 denominations: 1,3,5,10
+				 1. Objective function
+					 f(i) return the different number of ways to make change of size n.
 
-				2. Base cases
-					f(0) = 1;
+				 2. Base cases
+					 f(0) = 1;
 
-				3. transition function
-					f(n) += f(n-j(coin))'
+				 3. transition function
+					 f(n) += f(n-j(coin))'
 
-				4. Excution order
-					Bottom-up
+				 4. Excution order
+					 Bottom-up
 
-				5. answer location
-					f(n)
+				 5. answer location
+					 f(n)
 
-				Implemented by knowing our base cases, func boundaries, transition function, execution order and answer location.
+				 Implemented by knowing our base cases, func boundaries, transition function, execution order and answer location.
 
-				time complexity: O(n)
-				space complexity: O(n)
+				 time complexity: O(n)
+				 space complexity: O(n)
 		*/
 
 		coinChange := func(n int) int {
@@ -72,28 +72,28 @@ func main() {
 	// part 2: coin change with exactly (t) coins
 	{
 		/*
-			Coin change with exactly (t) coins
-			Given an unlimited supply of coins of given denominations(j), find the total number of ways(n) to make a change of size n bu using exactly 9(?) coins.
+			 Coin change with exactly (t) coins
+			 Given an unlimited supply of coins of given denominations(j), find the total number of ways(n) to make a change of size n bu using exactly 9(?) coins.
 
-			denominations: 1,2,3,5
-			1. Objective function
-				f(n,t) return the different number of ways to make change of size n by using exactly t(9) coins
+			 denominations: 1,2,3,5
+			 1. Objective function
+				 f(n,t) return the different number of ways to make change of size n by using exactly t(9) coins
 
-			2. Base cases
-				f(i,0) = 0
-				f(0,0) = 1
-				f(i,1) = 1 if we have the denomination.
+			 2. Base cases
+				 f(i,0) = 0
+				 f(0,0) = 1
+				 f(i,1) = 1 if we have the denomination.
 
-			3. Transition function
-				f(n) += f(n-coin, t-1)
+			 3. Transition function
+				 f(n) += f(n-coin, t-1)
 
-			4. Excution order
-				Bottom-up
+			 4. Excution order
+				 Bottom-up
 
-			5. Answer location f(n,t)
+			 5. Answer location f(n,t)
 
-			time complexity: O(n)
-			space complexity: O(n)
+			 time complexity: O(n)
+			 space complexity: O(n)
 		*/
 		coinChange := func(n, t int) int {
 			coins := []int{1, 2, 3, 5}
@@ -121,32 +121,30 @@ func main() {
 		coinChange(0, 0)
 	}
 
-	// part 3: coin change with no more than (t) coins
+	// part 2: coin change with no more than (t) coins
 	{
 		/*
-			Coin change with no more than (t) coins:
-			Given an unlimited supply of coins of given denominations, find the total number of ways to make a change of size n with no more than t coins.
+			 Coin change with no more than (t) coins:
+			 Given an unlimited supply of coins of given denominations, find the total number of ways to make a change of size n with no more than t coins.
 
-			denominations: 1,3,5,10
+			 denominations: 1,3,5,10
 
-			1. Objective function
-			f(n,t) return the num of ways to make a change of n by using no more than t coins.
+			 1. Objective function
+			 f(n,t) return the num of ways to make a change of n by using no more than t coins.
 
-			2. Base cases
-			f(i,0) = 0
-			f(i,1) = 1 if we have the denomination.
-			f(0,1nyNum) = 1, because 0 is acceptable as t.
+			 2. Base cases
+			 f(i,0) = 0
+			 f(i,1) = 1 if we have the denomination.
+			 f(0,1nyNum) = 1, because 0 is acceptable as t.
 
-			3. Transition function
-			f(n) += f(i-coin,j)
+			 3. Transition function
+			 f(n) += f(i-coin,j)
 
-			4. Execution order
-			bottom up
+			 4. Execution order
+			 bottom up
 
-			5. Answer location
-
-			2D array
-			sum of the all posibilities in a index.
+			 5. Answer location
+				 f(n,t)
 		*/
 
 		coinChange := func(n, t int) int {
@@ -175,7 +173,94 @@ func main() {
 		}
 
 		// fmt.Println(coinChange(3, 2))
-		fmt.Println(coinChange(10, 3))
+		// fmt.Println(coinChange(10, 3))
+		// fmt.Println()
+		coinChange(0, 3)
+	}
+
+	// part 3:
+	{
+		/*
+			 coin change:
+				 Given an unlimited supply of coins of given denominations,
+				 find the total number of ways to make a change of size n, by using an even number of coins.
+
+				 denominations: 1,3,5,10
+
+				 1. Objective function: Given n return the number of ways to make a change of size n with the given denomination by using even number of coins.
+				 f(i,x) 
+				 f(i,x) -> {
+					x: 1 == even
+					x: 0 == odd
+				 }
+
+				 2. Base Cases
+				 f(0,0) = 0
+				 f(0,1) = 1
+				 f(1,0) = 1
+				 f(1,1) = 0
+
+				 f(2,0) = 0
+				 f(2,1) = 1 -> (1*2)
+				 f(3,0) = 2 -> (1*3), 3(1)
+				 f(3,1) = 0
+				 f(4,0) = 0
+				 f(4,1) = 2 -> 1(1*4), 1(3 + 1),
+
+				 3. Transition function
+				 f(i,0) = f(i-coin, 1)
+				 f(i,1) = f(i-coin, 0)
+
+				 4. Execution order
+					 Bottom up
+
+				 5. Answer location
+					 f(n)
+
+		*/
+
+		coinChange := func(n int) int {
+			dp := make([][]int, n+1)
+			for i := range dp {
+				dp[i] = make([]int, 2)
+			}
+			dp[0][0] = 0
+			dp[0][1] = 1
+			// dp[1][0] = 1
+			// dp[1][1] = 0
+
+			// dp[2][0] = 0
+			// dp[2][1] = 1
+
+			// dp[3][0] = 2
+			// dp[3][1] = 0
+
+			// dp[4][0] = 0
+			// dp[4][1] = 2
+
+			// coins := []int{1,2,3,10}
+			coins := getCoins(n)
+			for i := 1; i <= n; i++ {
+				for _, coin := range coins {
+					if i-coin >= 0 {
+						dp[i][0] += dp[i-coin][1]
+						dp[i][1] += dp[i-coin][0]
+					}
+				}
+			}
+
+			return dp[n][1]
+		}
+
+		// fmt.Println(coinChange(0))
+		// fmt.Println(coinChange(1))
+		// fmt.Println(coinChange(2))
+		// fmt.Println(coinChange(3))
+		// fmt.Println(coinChange(4))
+		// fmt.Println(coinChange(6))
+		// fmt.Println(coinChange(4))
+		fmt.Println(coinChange(7))
+		fmt.Println(coinChange(8))
 
 	}
 }
